c12dfe0f5ab72e7657affa0d74c08e395d996caf
sis-I <sisshash@gmail.com>
2023-07-31 18:11:40 +0300

    Questions component modified

--- App.jsx
+++ App.jsx
@@ -1,39 +1,28 @@
 import React from "react";
 import IntroPage from "./components/IntroPage";
 import Questions from "./components/Questions";
-import Answers from "./components/Answers";
 
 import blobsTop from "./assets/blobs-top.svg";
 import blobsBottom from "./assets/blobs-bottom.svg";
 
 
 function App() {
+  const [showQuestions, setShowQuestions] = React.useState(false);
 
-    const [isIntro, setIsIntro] = React.useState(true);
-    const [isAnswerPage, setIsAnswerPage] = React.useState(false);
-
-    // const res = fetch("https://opentdb.com/api.php?amount=5");
-    // let data = res.json();
-    // console.log(data);
-
-    function handleClick(e) {
-      e.preventDefault();
-      setIsIntro(false);
-    }
-
-    function checkAnswer() {
-        setIsAnswerPage(true);
-    }
-
-    return (
-      <div className="app--background">
-        <img className="blobs-top" src={blobsTop} alt="Top Blobs" />
-        {isIntro && <IntroPage handleClick={handleClick}/>}
-        {!isIntro && !isAnswerPage && <Questions checkAnswer={checkAnswer}/>}
-        {isAnswerPage && <Answers /> }
-        <img className="blobs-bottom" src={blobsBottom} alt="Bottom Blobs" />
-      </div>
-    )
+  function getQuestions() {
+    setShowQuestions(true);
   }
+
+  return (
+    <div className="app--background">
+      <img className="blobs-top" src={blobsTop} alt="Top Blobs" />
+      { showQuestions ? 
+        <Questions  /> : 
+        <IntroPage handleClick={getQuestions} /> 
+      }
+      <img className="blobs-bottom" src={blobsBottom} alt="Bottom Blobs" />
+    </div>
+  )
+}
 
 export default App;
--- components/Questions.jsx
+++ components/Questions.jsx
@@ -1,45 +1,70 @@
 import React from "react"
 import ChoiceButton from "./ChoiceButton";
+import Answers from "./Answers";
 
 
 export default function Questions(props) {
     const [myData, setMyData] = React.useState([]);
-    const [selected, setSelected] = React.useState(Array(4).fill(false));
+    const [showAnswer, setShowAnswer] = React.useState(false);
+
+    const [selection, setSelection] = React.useState(getSelection());
 
     React.useEffect(() => {
         console.log("Effect runs");
         fetch("https://opentdb.com/api.php?amount=5")
             .then(res => res.json())
             .then(data => setMyData(data.results));
-
     }, []);
 
-    function answerClick(e, i) {
-        e.preventDefault();
-        console.log(i, "is clicked");
-        setSelected(prevSelected => prevSelected.map((select, index) => {
-            if (i === index) {
-              return !select;
-            }
-            return select;
-        }))
+    function getSelection() {
+        const selectArr = myData.map(data => {
+           return Array(data.incorrect_answers.length + 1).fill(false);
+        });
+        return selectArr;
+    }
+    
+    function checkAnswer() {
+        setShowAnswer(true);
     }
 
-    const questions = myData.map(quiz => {
-        const choiceElements = quiz.incorrect_answers.map((choice, index) => (
+
+    function answerClick(e, index, i) {
+        e.preventDefault();
+        console.log(i, "is clicked");
+        setSelection(prevSelection => prevSelection.map((ansList, selectIndex) => {
+            return ansList.map((choice, ansIndex) => {
+                if (index === selectIndex && i === ansIndex) {
+                    return !choice;
+                }
+                return choice;
+            })
+        }));
+    }
+    
+    function getChoices() {
+        const cpyData = myData.slice();
+        const choices = cpyData.map(singleObj => [...singleObj.incorrect_answers]);
+
+        return choices;
+    }
+
+    console.log(getChoices());
+
+    const questions = myData.map((quiz, index) => {
+        const choiceElements = quiz.incorrect_answers.map((choice, i) => (
             <ChoiceButton
-                key={index} 
-                choice={choice} 
-                handleClick={(e)=> answerClick(e, index)} 
-                selected={selected[index]} />
+                key={i}
+                choice={choice}
+                handleClick={(e) => answerClick(e, index, i)}
+                selected={"none"} />
         ));
         choiceElements.push(<ChoiceButton choice={quiz.correct_answer} />);
-        
+
         return (
             <div className="question-answer">
-                <h3>{ quiz.question }</h3>
+                <h3>{quiz.question}</h3>
                 <ul className="question-choices">
-                    { choiceElements }
+                    {choiceElements}
                 </ul>
                 <hr />
             </div>
@@ -47,14 +72,20 @@ export default function Questions(props) {
     });
 
     return (
-        <div className="question--main">
-            {questions}
-            <button 
-                className="check-ans--btn" 
-                onClick={props.checkAnswer}
-            >
-                Check Answer
-            </button>
-        </div>
+        <>
+        {!showAnswer ? 
+            <div className="question--main">
+                {questions}
+                <button
+                    className="check-ans--btn"
+                    onClick={checkAnswer}
+                >
+                    Check Answer
+                </button>
+            </div>
+            :
+            <Answers />
+        }   
+        </>
     )
 }
